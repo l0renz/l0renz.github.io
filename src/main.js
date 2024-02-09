3f43aa2eb21da6978814d59bdff578f40c0b863c
Lorenz Schmid <lorenz.schmid@student.tugraz.at>
2024-02-09 13:10:14 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+const content = Vue.createApp({
+  data: function () {
+    return {
+      pageTitle : 'MyPage'
+    }
+  }
+})
+
+// app.mount('#app')
